412524ac3954f97462d36f08ffba30a3a7b728c9
amir <amirbahraami99@gmail.com>
2025-07-10 21:28:42 +0330

    feat(main): add serial requests per connection

--- app/main.go
+++ app/main.go
@@ -11,39 +11,45 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	listener, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+	conn, err2 := listener.Accept()
+	if err2 != nil {
+		fmt.Println("Error accepting connection: ", err2.Error())
 		os.Exit(1)
 	}
 	defer conn.Close()
 
+	for {
+		handle(err2, conn)
+	}
+}
+
+func handle(err2 error, conn net.Conn) {
 	msgSizeArr := make([]byte, 4)
-	_, err = conn.Read(msgSizeArr)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
+	_, err2 = conn.Read(msgSizeArr)
+	if err2 != nil {
+		fmt.Println("Error reading from connection: ", err2.Error())
 		os.Exit(1)
 	}
 
 	msgSize := binary.BigEndian.Uint32(msgSizeArr)
 	reqHeader := make([]byte, msgSize)
-	_, err = conn.Read(reqHeader)
-	if err != nil {
-		fmt.Println("**Error reading from connection: ", err.Error())
+	_, err2 = conn.Read(reqHeader)
+	if err2 != nil {
+		fmt.Println("**Error reading from connection: ", err2.Error())
 		os.Exit(1)
 	}
 
 	req := MakeRequest(reqHeader, msgSize)
 	res := MakeResponse(req)
 
-	_, err = conn.Write(res.toByteArray())
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
+	_, err2 = conn.Write(res.toByteArray())
+	if err2 != nil {
+		fmt.Println("Error writing to connection: ", err2.Error())
 		os.Exit(1)
 	}
 }
